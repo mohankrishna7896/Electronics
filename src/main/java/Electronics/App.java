392255c83b6ba3d2844bf8566fba83cc446acc30
mohankrishna7896 <mohankrishna7896@gmail.com>
2022-03-18 17:26:47 +0530

    first commit

---
+++ src/main/java/Electronics/App.java
@@ -0,0 +1,13 @@
+package Electronics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
